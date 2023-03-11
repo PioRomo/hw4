803f5863548911c0a34766572f7e7ef62a5f32d9
PioRomo <104649165+PioRomo@users.noreply.github.com>
2023-03-11 11:08:20 -0800

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -10,17 +10,35 @@ class Circle(Base):
     def __init__(self, x, y, size):
         super().__init__(x,y,size)
     def draw(self):
-        return f""" ({self.x}, {self.y},
-        {self.size}),
-        circle here
+        return f""" ({self.x}, {self.y})
+        {self.size}
+                ___
+              --   --
+            --       --
+           -           -
+           -           -
+           -           -
+            --       --
+              --  --
 """
     
 class Square(Base):
     def __init__(self,x, y, size):
         super().__init__(x,y,size)
     def draw(self):
-        return f""" ({self.x}, {self.y},
-        {self.size}), square here"""
+        return f""" ({self.x}, {self.y})
+        {self.size}
+        ---------------------
+        -                   -
+        -                   -
+        -                   -
+        -                   -
+        -                   -
+        -                   -
+        -                   -
+        ---------------------
+        
+        """
     
 # All of the code below is correct
 def draw_any_shape(myShape):
