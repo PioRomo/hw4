6f19e8a1592d3a47269af2965fa2f75ffabafea7
PioRomo <104649165+PioRomo@users.noreply.github.com>
2023-03-10 23:18:01 -0800

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,16 @@
+def merge(firstList, secondList): 
+    len1 = len(firstList)
+    len2 = len(secondList)
+    i=0
+    j=0
+    res = []
+    
+    while i < len1 and j < len2: 
+        if firstList[i] < secondList[j]: 
+            res.append(firstList[i])
+            i+= 1
+        else: 
+            res.append(secondList[j])
+            j+=1
+    res = res + firstList[i:] + secondList[j:]
+    print(res)
