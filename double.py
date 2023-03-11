ab9f8b2f0ad626e894138d3693dfbd4efd10f12e
PioRomo <104649165+PioRomo@users.noreply.github.com>
2023-03-11 10:23:17 -0800

    Create double.py

---
+++ double.py
@@ -0,0 +1,19 @@
+
+def double(func): 
+    def inner():
+        #prints greet -> try that again -> greet again
+        func()
+        print("Lets try that again!")
+        func()
+    return inner
+
+
+def greet():
+    #print("Hello World")
+    from test import greet
+
+#sets double as the decorator and greet as the inner
+decoratedFunc = double(greet)
+decoratedFunc()
+
+    
