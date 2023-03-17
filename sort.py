f0e4685a8bd45bb770e67c55d5d4425f95ef7657
PioRomo <104649165+PioRomo@users.noreply.github.com>
2023-03-17 10:35:47 -0700

    Update sort.py

--- sort.py
+++ sort.py
@@ -2,8 +2,8 @@ def sort_dictionary(dict):
     newDict = sorted(dict.keys(), key=lambda x:dict[x][1])
     newnewDict = "["
     for name in newDict: 
-        newnewDict += "('" + name +"',"+ str(dict[name][0]) + "),"
-    newnewDict = newnewDict.rstrip(',')
+        newnewDict += "('" + name +"', "+ str(dict[name][0]) + "), "
+    newnewDict = newnewDict.rstrip(', ')
     newnewDict += "]"
     return newnewDict
     
